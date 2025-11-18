ac3af76725e38c32ae03383c63b452be727809da
iqbaladinur <iqbaladinur@gmail.com>
2025-11-18 09:21:10 +0700

    feat: enhance BackupModule and BackupService with TypeORM integration for Milestone management

--- src/modules/backup/backup.module.ts
+++ src/modules/backup/backup.module.ts
@@ -1,4 +1,5 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { BackupController } from "./controllers/backup.controller";
 import { BackupService } from "./services/backup.service";
 import { WalletsModule } from "../wallets/wallets.module";
@@ -6,9 +7,21 @@ import { CategoriesModule } from "../categories/categories.module";
 import { TransactionsModule } from "../transactions/transactions.module";
 import { MilestonesModule } from "../milestones/milestones.module";
 import { BudgetsModule } from "../budgets/budgets.module";
+import { Wallet } from "../wallets/entities/wallet.entity";
+import { Category } from "../categories/entities/category.entity";
+import { Transaction } from "../transactions/entities/transaction.entity";
+import { Budget } from "../budgets/entities/budget.entity";
+import { Milestone } from "../milestones/entities/milestone.entity";
 
 @Module({
-  imports: [WalletsModule, CategoriesModule, TransactionsModule, BudgetsModule, MilestonesModule],
+  imports: [
+    TypeOrmModule.forFeature([Wallet, Category, Transaction, Budget, Milestone]),
+    WalletsModule,
+    CategoriesModule,
+    TransactionsModule,
+    BudgetsModule,
+    MilestonesModule,
+  ],
   controllers: [BackupController],
   providers: [BackupService],
 })
